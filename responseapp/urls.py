d509c41e58e0ccac8e6537b3fbfa78cd35a956a9
pranavpkr <pranavpailkar144@gmail.com>
2019-09-16 21:07:21 +0530

    Initial commit

--- responseapp/urls.py
+++ responseapp/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 from responseapp import views as responseapp_views
 
 urlpatterns = [
- path('response/', responseapp_views.responseform),
- path('thankyou/', responseapp_views.responseform),
-
-path('', admin.site.urls),
+    path('response/', responseapp_views.responseform),
+    path('', admin.site.urls),
 ]
